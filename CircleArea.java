a7f681e4d010c3276e4feb0ec19873a8aab70c9f
lekhyapadala6-prog <lekhyapadala6@gmail.com>
2026-08-29 15:32:10 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public  class CircleArea{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius");
+        double radius=sc.nextDouble();
+        double area=Math.PI*radius*radius;
+        System.out.println("Area of the Cirrcle:"+area);
+        sc.close(); 
+
+    }
+}
