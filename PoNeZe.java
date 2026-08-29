0ff9d437111a2e44c5abfa472fb5613543254136
lekhyapadala6-prog <lekhyapadala6@gmail.com>
2026-08-29 16:29:23 +0530

    Add files via upload

---
+++ PoNeZe.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PoNeZe{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        if(num>0){
+            System.out.println("Number is positive");
+        }
+        else if(num<0){
+            System.out.println(num + " is negative");
+        }
+        else{
+            System.out.println(num + " is zero");
+        }
+        sc.close();
+    }
+}
