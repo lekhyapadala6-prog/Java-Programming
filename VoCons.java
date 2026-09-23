4f16f740939435984dd37406af8560acd8dadbf0
lekhyapadala6-prog <lekhyapadala6@gmail.com>
2026-09-23 11:29:44 +0530

    Add files via upload

---
+++ VoCons.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class VoCons{
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Character:");
+        char ch=sc.next().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            System.out.println(ch  +  " it is a Vowel");
+        }
+        else{
+            System.out.println(ch  +  " it is a consonant");
+        }
+    }
+}
